d464a43eb505a97eba8d91c5ea19bff3b75adcdf
Kai Volland <kaivolland@yahoo.de>
2024-08-30 10:31:59 +0200

    feat: introduce SectionHeader component

---
+++ simulationTool/components/SectionHeader.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+    name: "SectionHeader",
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      icon: {
+        type: String,
+        required: false
+      }
+    }
+}
+</script>
+
+<template>
+  <header class="section-header">
+    <i
+      v-if="icon"
+      class="bi"
+      :class="icon"
+    ></i>
+    <h3>{{ title }}</h3>
+    <div class="actions">
+      <slot name="actions"></slot>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    display: flex;
+    align-items: center;
+    font-size: 1.75rem;
+    padding: .5rem;
+    border-bottom: 2px solid var(--bs-border-color);
+    margin-bottom: 2rem;
+
+    i {
+      margin-right: .75rem;
+    }
+
+    h3 {
+      flex: 1;
+      margin-bottom: 2px
+    }
+  }
+
+  .actions {
+    display: flex;
+    gap: 8px;
+  }
+</style>
